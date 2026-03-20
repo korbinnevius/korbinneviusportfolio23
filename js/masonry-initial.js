1c500e4f90a405f6eb435ad1637953ca01890bf2
Korbin <klnevius@gmail.com>
2026-03-20 14:47:15 -0400

    masonry intitialization tweaks

--- js/masonry-initial.js
+++ js/masonry-initial.js
@@ -3,6 +3,9 @@
 // init Masonry
 var grid = document.querySelector('.grid');
 
+grid.style.opacity = '0';
+grid.style.transition = 'opacity 0.4s ease';
+
 var msnry = new Masonry(grid, {
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
@@ -10,6 +13,7 @@ var msnry = new Masonry(grid, {
     // gutter: 5
 });
 
-imagesLoaded(grid).on('progress', function () {
+imagesLoaded(grid).on('always', function () {
     msnry.layout();
+    grid.style.opacity = '1';
 });
